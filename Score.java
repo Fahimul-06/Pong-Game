29dc61c8ca21f6a6c2cb69f6264f024eb1a957b9
Fahimul_Arefin <124819732+Fahimul-06@users.noreply.github.com>
2025-04-20 23:52:19 +0600

    Score.java

--- Score.java
+++ Score.java
@@ -1,26 +1,33 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
-import javax.swing.*;
 
 public class Score extends Rectangle {
 
-	static int GAME_WIDTH;
-	static int GAME_HEIGHT;
-	int player1;
-	int player2;
-	
-	Score(int GAME_WIDTH, int GAME_HEIGHT){
-		Score.GAME_WIDTH = GAME_WIDTH;
-		Score.GAME_HEIGHT = GAME_HEIGHT;
-	}
-	public void draw(Graphics g) {
-		g.setColor(Color.white);
-		g.setFont(new Font("Consolas",Font.PLAIN,60));
-		
-		g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
-		
-		g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10),(GAME_WIDTH/2)-85,50);
-		g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10),(GAME_WIDTH/2)+20,50); 
-	}
+    static int GAME_WIDTH;
+    static int GAME_HEIGHT;
+    int player1;
+    int player2;
+    int highScore;
+
+    Score(int width, int height) {
+        Score.GAME_WIDTH = width;
+        Score.GAME_HEIGHT = height;
+    }
+
+
+    public void draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("Consolas", Font.BOLD, 50));
+        g.drawLine(GAME_WIDTH/2, 0, GAME_WIDTH/2, GAME_HEIGHT);
+        g.drawString(String.valueOf(player1), (GAME_WIDTH/2) - 85, 50);
+        g.drawString(String.valueOf(player2), (GAME_WIDTH/2) + 40, 50);
+
+        highScore = Math.max(ScoreManager.loadHighScore(), Math.max(player1, player2));
+        if (Math.max(player1, player2) > ScoreManager.loadHighScore()) {
+            ScoreManager.saveHighScore(Math.max(player1, player2));
+        }
+
+        g.setFont(new Font("Arial", Font.PLAIN, 20));
+        g.drawString("High Score: " + highScore, (GAME_WIDTH/2) - 70, GAME_HEIGHT - 20);
+    }
+
 }
